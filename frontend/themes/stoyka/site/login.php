5213ec4b1bf717f3d04a10cf723eb3072fc5a800
agronom2005 <agronom20005@gmail.com>
2020-03-31 17:53:13 +0300

    update login page

--- frontend/themes/stoyka/site/login.php
+++ frontend/themes/stoyka/site/login.php
@@ -7,35 +7,42 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
+$this->title = 'Вход на сайт';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+<section>
+    <div class="content">
+        <!-- <div class="clear">
+            <div class="breadcrumbs left">
+                <ul>
+                    <li><a href="/">Главная</a></li><li><?//= Html::encode($this->title) ?></li>
+                </ul>
+            </div>
+        </div> -->
+        <p class="products__title"><?= Html::encode($this->title) ?></p>
+        <div class="block clear login-block">
+            <?php $form = ActiveForm::begin(['id' => 'login-form', 'class' => 'clear']); ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('Email: ') ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+                <?= $form->field($model, 'password')->passwordInput()->label('Пароль:: ') ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                <div class="login-checkbox clear">
+                    <?= $form->field($model, 'rememberMe')->checkbox()->label('Запомнить меня') ?>
+                </div>
+                <?= Html::a('Забыли пароль?', ['site/request-password-reset']) ?>.
 
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                    <br>
-                    Need new verification email? <?= Html::a('Resend', ['site/resend-verification-email']) ?>
+                <div style="color:#999;margin:1em 0;text-align:center">
+                    Забыли подтвердить email? <?= Html::a('Отправить повторно', ['site/resend-verification-email']) ?>
                 </div>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= Html::submitButton('Войти', ['class' => 'button', 'name' => 'login-button']) ?>
                 </div>
 
             <?php ActiveForm::end(); ?>
         </div>
     </div>
-</div>
+</section>

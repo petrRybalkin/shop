0dade28c9ba0036ba97edbcbc515dff6b12e8185
RSol <slava.rudnev@gmail.com>
2020-04-01 09:34:56 +0300

    Order

--- backend/views/order/index.php
+++ backend/views/order/index.php
@@ -41,7 +41,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
             [
                 'class' => 'yii\grid\ActionColumn',
-                'template' => '{view} {delete}',
+                'template' => '{view}',
             ],
         ],
     ]); ?>

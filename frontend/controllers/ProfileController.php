7f8df45305a38ab229f8e59511855d76dee4c004
RSol <slava.rudnev@gmail.com>
2020-04-05 18:47:36 +0300

    Self orders

---
+++ frontend/controllers/ProfileController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace frontend\controllers;
+
+use common\models\Order;
+use common\models\OrderSearch;
+use Yii;
+use yii\filters\AccessControl;
+use yii\helpers\ArrayHelper;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\web\NotFoundHttpException;
+
+/**
+ * ProductController implements the CRUD actions for Product model.
+ */
+class ProfileController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        $searchModel = new OrderSearch();
+        $params = Yii::$app->request->queryParams;
+        ArrayHelper::setValue($params, 'OrderSearch.user_id', Yii::$app->user->getId());
+        $dataProvider = $searchModel->search($params);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Order model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Finds the Order model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Order the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Order::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
---
+++ frontend/themes/stoyka/profile/index.php
@@ -0,0 +1,46 @@
+<?php
+
+use common\models\Order;
+use yii\grid\GridView;
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\OrderSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Ваши заказы';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="order-index">
+
+    <h1><?= $this->title ?></h1>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            'id',
+            [
+                'attribute' => 'status',
+                'filter' => Order::statusList(),
+                'format' => 'raw',
+                'value' => function (Order $model) {
+                    return Html::a($model->getStatusTag([
+                            'class' => 'hz hz-' . $model->getStatusColor(),
+                    ]), ['view', 'id' => $model->id]);
+                }
+            ],
+            'name',
+            'phone',
+            'city',
+            'address',
+            'price',
+            'delivery',
+            'description',
+            'created_at:datetime',
+            //'updated_at',
+        ],
+    ]); ?>
+
+
+</div>
